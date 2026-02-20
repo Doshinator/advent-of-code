849c436151609f455fa6d6da5a4cb808abe800e0
Rushabh Doshi <rush3doshi@gmail.com>
2026-02-19 19:56:13 -0500

    part 1 done

--- advent-2015/src/day4.rs
+++ advent-2015/src/day4.rs
@@ -1,15 +1,17 @@
+use md5::{Digest, Md5};
+
 use crate::config::Config;
 
 pub fn solve(config: &Config) -> std::io::Result<()> {
-    let path = config.input_path_str();
+    let input: &str = "iwrupvqb";
 
-    match config.day {
+    match config.part {
         1 => {
-            let result = solve_part1(path);
+            let result = solve_part1(input);
             println!("Part 1 result: {}", result);
         },
         2 => {
-            let result = solve_part2(path);
+            let result = solve_part2(input);
             println!("Part 2 result: {}", result);
         },
         _ => eprintln!("Day {} Part {} not implemented", config.day, config.part),
@@ -18,10 +20,20 @@ pub fn solve(config: &Config) -> std::io::Result<()> {
     Ok(())
 }
 
-fn solve_part1(path: &str) -> i32 {
-    todo!()
+fn solve_part1(key: &str) -> i32 {
+    let mut counter = 1;
+    loop {
+        let mut hash = Md5::new();
+        hash.update(format!("{}{}", key, counter));
+        let result = hash.finalize();
+
+        if result[0] == 0 && result[1] == 0 && (result[2] & 0xf0) == 0 {
+            return counter;
+        }
+        counter += 1;
+    }
 }
 
-fn solve_part2(path: &str) -> i32 {
+fn solve_part2(key: &str) -> i32 {
     todo!()
 }
