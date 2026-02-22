62d81ea9e8edb14d212e51da81a2182eaea70824
Rushabh Doshi <rush3doshi@gmail.com>
2026-02-21 20:59:37 -0500

    part 1 done. Rewritten to a high aesthetic standard, using .filter().count(), matches!() and chars().zip()

--- advent-2015/src/day5.rs
+++ advent-2015/src/day5.rs
@@ -1,10 +1,16 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 use crate::config::Config;
 
 pub fn solve(config: &Config) -> std::io::Result<()> {
+    let path = config.input_path_str();
+
     match config.part {
         1 => {
-            let result = solve_part_1();
-            println!("Day {} Part {} result: {}", config.day, config.part, result);
+            let result = solve_part_1(path);
+            println!("Day {} Part {}. Total nice strings:{}", config.day, config.part, result.unwrap());
         },
         2 => {
             let result = solve_part_2 ();
@@ -15,10 +21,50 @@ pub fn solve(config: &Config) -> std::io::Result<()> {
     Ok(())
 }
 
-fn solve_part_1() -> i32 {
-    todo!()
+fn solve_part_1(path: &str) -> std::io::Result<i32> {
+    let lines = read_lines(path)?;
+    let result = lines
+        .map_while(Result::ok)
+        .filter(|line| nice_str(line))
+        .count() as i32;
+        
+    Ok(result)
 }
 
 fn solve_part_2() -> i32 {
     todo!()
+}
+
+fn nice_str(s: &str) -> bool {
+    if !contains_three_vowels(s) {
+        return false;
+    }
+
+    let mut has_double = false;
+    
+    for (a, b) in s.chars().zip(s.chars().skip(1)) {
+        // Forbidden pairs
+        if matches!((a, b), 
+            ('a','b') | ('c','d') | ('p','q') | ('x','y')) {
+            return false;
+        }
+        if a == b {
+            has_double = true;
+        }
+    }
+
+    has_double
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn contains_three_vowels(s: &str) -> bool {
+    let vowels = ['a', 'e', 'i', 'o', 'u',];
+    s.chars()
+        .filter(|c| vowels.contains(c))
+        .count() >= 3
 }
